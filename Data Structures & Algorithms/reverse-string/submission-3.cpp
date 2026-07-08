b7d876261d3a234395861e72894301e90d2503e6
Ashish Upadhyay <137411624+ash18here@users.noreply.github.com>
2026-07-08 23:27:05 +0530

    Add: reverse-string - submission-3

---
+++ Data Structures & Algorithms/reverse-string/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st=0;
+        int l=s.size()-1;
+        char temp;
+        while(st<l){
+            temp=s[st];
+            s[st]=s[l];
+            s[l]=temp;
+            st++;
+            l--;
+            
+
+        }
+
+
+        
+    }
+};
